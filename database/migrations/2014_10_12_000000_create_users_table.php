1d7685cbc4fdb0f74bf3cb5310aae6c7ddb638fa
T$uru$hima <67181317+SotaTsurushima@users.noreply.github.com>
2023-02-11 19:02:37 +0900

    add img_url column for users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('img_url', 128)->nullable(false);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
